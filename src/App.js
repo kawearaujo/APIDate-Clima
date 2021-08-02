ffd6a9ed89cfcada45ed9d85ae8e4bd5957d2a90
kawearaujo <kawe.apr@gmail.com>
2021-08-02 10:12:45 -0300

    by Kawe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather= async(lat,long) => {
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+    console.log(res.data);
+  }
+
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition((position)=> {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    })
+  },[])
+  
+  if (location == false) {
+    return(
+      <Fragment>
+        <h3>Clima nas suas Coordenadas (N/A)</h3>
+        <hr/>
+        <ul>
+          <li>Temperatura atual: N/A</li>
+          <li>Temperatura máxima: N/A</li>
+          <li>Temperatura minima: N/A</li>
+          <li>Pressão: N/A</li>
+          <li>Umidade: N/A</li>
+        </ul>
+      </Fragment>
+    );
+  }else{
+    return (
+      <Fragment>
+        <h3>Resumo atual do clima em {weather['name']}[{weather['sys']['country']}] :</h3>
+        <hr />
+        <ul>
+          <li>O céu está com: {weather['weather'][0]['description']}</li>
+          <li>Temperatura atual: {weather['main']['temp']}°</li>
+          <li>Velocidade do Vento: {weather['wind']['speed']}km/h</li>
+          <li>Pressão: {weather['main']['pressure']} hpa</li>
+          <li>Umidade: {weather['main']['humidity']}%</li>
+        </ul>
+      </Fragment>
+    );
+  }
+
+  
 }
 
 export default App;
